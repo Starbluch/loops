9343ad494278c1ce90b776598f6ab86c20582d08
admin <gadzukoleksandr29@gmail.com>
2024-11-30 20:39:34 +0200

    commit

--- main.cpp
+++ main.cpp
@@ -4,8 +4,6 @@ using namespace std;
 
 int main()
 {
-    /*Напишіть програму, яка обчислює суму цілих чисел від а до 500 (значення a вводиться з клавіатури).*/
-
     int start_number, sum = 0;
     cout << "Enter start number: ";
     cin >> start_number;
